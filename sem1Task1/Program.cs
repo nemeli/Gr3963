64c507002be2a4e7796f2bc741e70bdeda5dca5b
nemi <nemelida@bk.ru>
2023-02-14 00:01:54 +0300

    second prog

---
+++ sem1Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write number");
+string numLine1 = Console.ReadLine();
+
+string numLine2 = Console.ReadLine();
+
+if (numLine1 != null && numLine2 != null) ;
+{
+    int num1  = int.Parse(numLine1);
+
+    int num2 = int.Parse(numLine2);
+
+    if (num2*num2 == num1)
+
+        {
+        Console.WriteLine("Yes");
+        }
+
+    else
+        {
+        Console.WriteLine("No");
+        } 
+}
