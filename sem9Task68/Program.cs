7e5d7c8824710a8db1bd552271e465aefa60a153
nemi <nemelida@bk.ru>
2023-03-13 23:08:36 +0300

    9 семинар дополнение

--- sem9Task68/Program.cs
+++ sem9Task68/Program.cs
@@ -13,6 +13,13 @@
         return AkkermanFunk(n-1,AkkermanFunk(n,m-1));
     }
 }
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
 
-int res =AkkermanFunk(3,0);
+int numAkkerN = ReadData("напишите первое число ");
+int numAkkerM = ReadData("напишите второе число ");
+int res =AkkermanFunk(numAkkerN,numAkkerM);
 Console.WriteLine(res);
